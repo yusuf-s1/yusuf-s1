8df86db4f4e6341fa8a1d9b36134725f831ae28c
Yusuf Solmaz <117117382+yusuf-s1@users.noreply.github.com>
2023-12-16 14:13:20 +0300

    Delete src directory

--- src/pages/Articles.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Articles() {
-  return (
-    <>
-      <div>Lorem, ipsum dolor.2488888</div>
-    </>
-  );
-}
--- src/pages/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function NotFound() {
-  return (
-    <>
-      <div className="notFound">Not Found</div>
-    </>
-  );
-}
